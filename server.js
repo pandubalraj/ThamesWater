9bcfbc802dfd5d8bfd931356fd926b068f531edf
pandubalraj <pandubalraj@gmail.com>
2016-12-02 09:26:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,17 +17,17 @@ var bot = new builder.UniversalBot(connector);
 // // // Setup Restify Server
 var server = restify.createServer();
 // Handle Bot Framework messages
-server.post('/api/messages', connector.listen());
+
 // Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
+//server.get(/.*/, restify.serveStatic({
+//	'directory': '.',
+//	'default': 'index.html'
+//}));
 
 server.listen(process.env.port|| process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
-
+server.post('/api/messages', connector.listen());
 // Create bot root dialog
 bot.dialog('/', dialog);
 
