05b66f7552133254c38b8e5ff2c20a65d6f704f4
pandubalraj <pandubalraj@gmail.com>
2016-12-02 12:00:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var sourceFile = require('./sourceFile');
 var recognizer1 = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/1d787b4e-3a61-4da2-ae05-f050e7e39d0b?subscription-key=c9ad898006c6426d95251f015167aaa1&q=');
 var intents  = new builder.IntentDialog({ recognizers: [recognizer1] });
 
+// // // Setup Restify Server
+var server = restify.createServer();
+// Handle Bot Framework messages
+
+server.listen(process.env.port|| process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
 // Get secrets from server environment
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
@@ -13,20 +20,6 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector);
-
-// // // Setup Restify Server
-var server = restify.createServer();
-// Handle Bot Framework messages
-
-// Serve a static web page
-//server.get(/.*/, restify.serveStatic({
-//	'directory': '.',
-//	'default': 'index.html'
-//}));
-
-server.listen(process.env.port|| process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
 server.post('/api/messages', connector.listen());
 // Create bot root dialog
 bot.dialog('/', intents);
